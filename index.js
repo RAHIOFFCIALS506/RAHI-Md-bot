a3b960def8d5625efcfaa532bc9edf8faaba3383
Rahi officials <rahiofficials506@gmail.com>
2026-09-22 15:13:17 +0600

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,52 @@
 import { SessionManager } from "./lib/manager.js";
 import initializeTelegramBot from "./lib/telegram-bot.js";
 import { loadCommands } from "./commands/index.js";
+import { generalCommands } from "./general.js";
+import { adminCommands } from "./admin.js";
+import { mediaCommands } from "./media.js";
+import { downloadCommands } from "./download.js";
+import { toolsCommands } from "./tools.js";
+import { aiCommands } from "./ai.js";
+import { funCommands } from "./fun.js";
+import { islamicCommands } from "./islamic.js";
+import { groupCommands } from "./group.js";
+import { ownerCommands } from "./owner.js";
+import { converterCommands } from "./converter.js";
+import { stickerCommands } from "./sticker.js";
+import { searchCommands } from "./search.js";
+import { animeCommands } from "./anime.js";
+import { textmakerCommands } from "./textmaker.js";
+import { logoCommands } from "./logo.js";
 
+export async function loadCommands(manager) {
+  const all = [
+    ...generalCommands,
+    ...adminCommands,
+    ...mediaCommands,
+    ...downloadCommands,
+    ...toolsCommands,
+    ...aiCommands,
+    ...funCommands,
+    ...islamicCommands,
+    ...groupCommands,
+    ...ownerCommands,
+    ...converterCommands,
+    ...stickerCommands,
+    ...searchCommands,
+    ...animeCommands,
+    ...textmakerCommands,
+    ...logoCommands,
+  ];
+
+  const map = new Map();
+  for (const cmd of all) {
+    for (const alias of cmd.aliases) {
+      map.set(alias.toLowerCase(), cmd);
+    }
+  }
+  manager.commands = map;
+  console.log(`📦 Loaded ${all.length} commands (${map.size} aliases)`);
+}
 const manager = new SessionManager();
 
 // Load all WhatsApp commands
